4a42e7ecbe47afbafbc5dc123cac011de39e8d70
Howard Deiner <howard.deiner@deinersoft.com>
2017-08-29 13:51:35 -0400

    Initial commit.

--- src/main/java/com/deinersoft/timeteller/ClockLocal.java
+++ src/main/java/com/deinersoft/timeteller/ClockLocal.java
@@ -6,7 +6,7 @@ public class ClockLocal extends Clock {
     LocalDateTime localDateTime = null;
 
     public ClockLocal() {
-        timeZone = TimeZone.UTC;
+        timeZone = TimeZone.LOCAL;
         localDateTime = LocalDateTime.now();
     }
 
